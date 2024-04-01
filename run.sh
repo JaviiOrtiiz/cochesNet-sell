60776f736279c35f94c0e6a6b5106986953581fb
Javi Ortiz <javiortizinthewall@gmail.com>
2024-04-01 19:19:32 +0200

    added -p docker run

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -v /home/pablo/cochesNet-sell:/app --restart always cochesnet-sell --name cochesnet
+docker run -d -v /home/pablo/cochesNet-sell:/app --restart always cochesnet-sell --name cochesnet -p 5000:5000
