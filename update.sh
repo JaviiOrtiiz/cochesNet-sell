b9651c5dc1e8e63787cf9d9ba0ebd0de4e1ce31f
Javi Ortiz <javiortizinthewall@gmail.com>
2024-04-03 20:32:03 +0200

    added autoupdate sh code

---
+++ update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Navigate to the directory containing your repository
+cd /home/pablo/cochesNet-sell
+
+# Pull the latest changes from the Git repository
+git pull
+
+# Stop and remove the old container if it exists
+if [ "$(docker ps -q -f name=cochesnet-sell)" ]; then
+    docker stop cochesnet-sell
+    docker rm cochesnet-sell
+fi
+
+# Remove the old Docker image if it exists
+if [ "$(docker images -q cochesnet-sell)" ]; then
+    docker rmi cochesnet-sell
+fi
+
+# Build the Docker image
+docker build -t cochesnet-sell .
+
+# Run the run.sh script
+sh run.sh
