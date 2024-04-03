93bb450af78087832465f5c0c2c371c3d8dd6e76
Javi Ortiz <javiortizinthewall@gmail.com>
2024-04-03 20:39:23 +0200

    removed port bind

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -v /home/pablo/cochesNet-sell:/app --restart always --name cochesnet-sell -p 5000:5000 cochesnet-sell
+docker run -d -v /home/pablo/cochesNet-sell:/app --restart always --name cochesnet-sell cochesnet-sell
